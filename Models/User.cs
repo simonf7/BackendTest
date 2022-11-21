49106012c365774fcde62a3b3a059c5aaa0f354f
Simon Foster <simonf7@hotmail.com>
2022-11-21 19:13:32 +0000

    [feature] add initial User model and controller scaffolding

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace BackendTest.Models;
+
+public class User
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using BackendTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(opt =>
+    opt.UseInMemoryDatabase("User"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
